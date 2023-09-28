43fdd1fec1843344a8d97676d1d3be94550075c4
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-09-28 01:56:25 -0300

    feat: configure admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', # Divide os dados dos contatos
+    ordering = 'id', # Exibe o id de cada contato
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name' # Cria um icone d busca 
+    list_per_page = 10
+    list_max_show_all = 200
+    # list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+'''
+id (primary key - automático)
+coisas que irei usar para os contatos: 
+firrst_name (string), last_name (string), phone (string),
+email (email), created_date (date), description (text)
+category (foreign key), show (boolean), owner (foreing key),
+picture (imagem).
+'''
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
