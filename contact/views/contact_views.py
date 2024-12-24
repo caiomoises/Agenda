13503dc5742b08893dc07606a8de3c0b71a903a0
caiomoises <caio.cavalcante@alunos.ufersa.edu.br>
2024-12-24 19:42:51 -0300

    feat: Add paginator

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from ..models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
     contacts = Contact.objects.filter(
         show=True,
-    )[:10]
+    )
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -34,8 +39,12 @@ def search(request):
         show=True,
     )
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Pesquisa - ',
     }
 
